264e847bc60289f823fef28bc5d6fd275c6cf4ea
haniffakhri <hanifretbock@gmail.com>
2023-12-22 00:13:33 +0700

    install reactjs and tailwindcss and create first template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/*EXTENTION : 
+1. AUTO RENAME TAG
+2. ES7+REACT/REDUX/REACT-NATIVE SNIPPETS
+3. CONSOLE NINJA
+4. GITHUB COPILOT AND GITHUB COPILOT CHAT ( OPSIONAL )
+5. PRETTIER
+6. TAILWINDCSS INTELISENSE
+*/
+import React from 'react'  // mengimpor react React
+
+export default function App() {
+  return (
+    <h1 className=' text-red-300'>App</h1>
+  )
+}
