c69172d7daa8b5edbbf146ab60142ff0d563a017
haniffakhri <hanifretbock@gmail.com>
2023-12-22 00:28:17 +0700

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,21 @@
 5. PRETTIER
 6. TAILWINDCSS INTELISENSE
 */
-import React from 'react'  // mengimpor react React
+import { BrowserRouter, Routes, Route } from "react-router-dom"  // mengimpor react React-router-dom.     cara instal: npm i react-router-dom
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Home from "./pages/Home"
 
 export default function App() {
-  return (
-    <h1 className=' text-red-300'>App</h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
 }
