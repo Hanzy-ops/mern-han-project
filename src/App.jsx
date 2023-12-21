d89db04f51cbc61edd64ab4aff770776ad90edbc
haniffakhri <hanifretbock@gmail.com>
2023-12-22 01:21:39 +0700

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,28 @@
 5. PRETTIER
 6. TAILWINDCSS INTELISENSE
 */
-import { BrowserRouter, Routes, Route } from "react-router-dom"  // mengimpor react React-router-dom.     cara instal: npm i react-router-dom
-import Signin from "./pages/Signin"
-import Signup from "./pages/Signup"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // mengimpor react React-router-dom.     cara instal: npm i react-router-dom
+// LINK NAVIGASI
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<Signin />} />
-      <Route path="/sign-up" element={<Signup />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+  return (
+    <BrowserRouter>
+    {/* Menghubungkan Component Headers */}
+      <Header />
+      {/* Navigasi Menu */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
+  );
 }
